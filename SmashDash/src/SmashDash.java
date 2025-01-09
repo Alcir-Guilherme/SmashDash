8e36cdf326e4e4d5e46f8a7c48f35aa1eccef5fb
Alcir-Guilherme <134345378+Alcir-Guilherme@users.noreply.github.com>
2025-01-09 17:46:18 -0300

    Add files via upload

--- SmashDash/src/SmashDash.java
+++ SmashDash/src/SmashDash.java
@@ -47,12 +47,31 @@ public class SmashDash extends JPanel implements ActionListener{
         jogadorPulando =new ImageIcon(getClass().getResource("./imagens/jogadorpulando.png")).getImage();
         jogadorAgachando = new ImageIcon(getClass().getResource("./imagens/jogadoragachado.png")).getImage();
 
+        ImageIcon ateroid = new ImageIcon(getClass().getResource("./imagens/asteroid.png"));
+        ImageIcon asteroid = new ImageIcon(getClass().getResource("/imagens/asteroidInvocado.png"));
+
         //criando o jogador
         jogador = new Block(jogadorX,jogadorY,jogadorLargura,jogadorAltura,jogadorCorrendo);
-
+        asteroide = new Block(asteroidX, asteroidY, asteroidLargura, asteroidAltura, Asteroid);
         //game loop (aqui esta sendo feito a repeticao da imagem do jogador correndo
         gameLoop = new Timer(1000/60,this); // um looping de 1 sec
         gameLoop.start();
+
+        JButton butao = new JButton(ateroid);
+        butao.setToolTipText("Olha a pedra!");
+        butao.setEnabled(true);
+
+        // Adicionando o botão ao painel
+        add(butao);
+        setLayout(new FlowLayout(FlowLayout.RIGHT));
+        // Adicionando um ActionListener ao botão
+        butao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Ação a ser realizada quando o botão for clicado
+                repaint();
+            }
+        });
 
     }
     //chamando a funcao do JPanel
@@ -60,6 +79,10 @@ public class SmashDash extends JPanel implements ActionListener{
         super.paintComponent(g);
         desenhando(g);
     }
+    public void desenhado_ateroid(Graphics g){
+        g.drawImage(a)
+    }
+
     public void desenhando(Graphics g){
         g.drawImage(jogador.imagem, jogador.x, jogador.y, jogador.largura, jogador.altura, null);
     }
